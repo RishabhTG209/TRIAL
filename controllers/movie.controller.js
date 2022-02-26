ed2c745968e0927793c154d5b27f6cea467b0441
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-02-26 16:40:24 +0530

    Update movie.controller.js

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -14,6 +14,48 @@ router.post("/",async(req,res)=>{
     }
 })
 
+router.post("/additem/:user_id", async (req,res)=>{
+
+    try{
+        const product_id=req.body.product_id;
+        const user_id = req.params.user_id;
+
+        const cart_item= await cart.findOne({"user_id":user_id,"product_id":product_id}).lean().exec();
+
+        
+         
+        if(cart_item){
+
+            if(req.query.operation=="dec"){
+
+                await cart.findByIdAndUpdate(cart_item._id,{$inc:{qty:-1}});
+             
+            }
+            else{
+
+               
+                await cart.findByIdAndUpdate(cart_item._id,{$inc:{qty:1}});
+              
+            }
+            res.end() ;   
+
+        }
+        else{
+           
+           await cart.create(req.body);
+           res.end()
+         }
+
+     
+
+    }catch(e){
+
+        res.send(e.message)
+
+    }
+
+});
+
 router.get("/",async(req,res)=>{
     try {
         const movies = await Movie.find().lean().exec()
